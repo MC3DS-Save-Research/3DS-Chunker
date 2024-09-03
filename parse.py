3875c2145f4a4107a33eedc1a161e32b88ab6f30
Anonymous941 <36797492+Anonymous941@users.noreply.github.com>
2024-09-03 16:10:05 -0400

    lots of improvements

--- parse.py
+++ parse.py
@@ -2,6 +2,7 @@
 
 import sys
 from pathlib import Path
+import shutil
 import re
 import traceback
 
@@ -13,34 +14,34 @@ DEFINITION = """
 #define MAGIC_CDB 0xABCDEF98
 #define MAGIC_VDB 0xABCDEF99
 
-struct commonHeader {
-    // I think these are 16-bit, and are usually 1
-    uint16 unknown0;
-    uint16 unknown1;
+struct FileHeader {
+    // both are always 1
+    uint16 something0;
+    uint16 something1;
+    uint32 subfileCount; // total number of subfiles
+    uint32 unknown0;
+    uint32 subfileSize; // size of each subfile
+    uint32 unknown1;
+};
 
-    uint8 unknownBitmask; // USUALLY only has one bit set at a time, for some reaso
-    uint8 unknown2[0xF];
+struct SubfileHeader {
+    uint8 unknown0[sizeof(FileHeader)]; // the first subfile has a file header and the rest have garbage here
     uint32 magic;
-    uint8 unknown3[0x8];
+    uint8 unknown1[8]; // unknown
 };
 
-struct chunkSection {
-    // -1 = missing
-    int32 index;
-    int32 compressedSize;
-    int32 decompressedSize;
-    uint32 unknown;
+struct ChunkSection {
+    int32 index; // -1 = empty
+    int32 compressedSize; // -1 = empty
+    int32 decompressedSize; // 0 = empty
+    int32 unknown; // 0 = empty
 };
 
-struct chunkHeader {
-    commonHeader common;
-    uint32 unknown;
-    chunkSection sections[6];
-};
-
-struct chunk {
-    chunkHeader header;
-    uint8 data[0x2800-0x84];
+struct ChunkHeader {
+    SubfileHeader subfileHeader;
+    int16 unknown0;
+    int16 unknown1;
+    ChunkSection sections[6];
 };
 """
 parser = cstruct().load(DEFINITION)
@@ -52,34 +53,48 @@ def size_check(struct, expected_size, name):
     ), f"size of {name} is 0x{struct.size:X}, should be 0x{expected_size:X}"
 
 
-size_check(parser.commonHeader, 0x20, "common header")
-size_check(parser.chunkHeader, 0x84, "chunk header")
-size_check(parser.chunk, 0x2800, "chunk")
+size_check(parser.FileHeader, 0x14, "file header")
+size_check(parser.SubfileHeader, 0x20, "subfile header")
+size_check(parser.ChunkHeader, 0x84, "chunk header")
 
 
-def parse_chunk(raw_chunk):
-    parsed = parser.chunk(raw_chunk)
+def parse_chunk(stream, subfile_size):
+    header = parser.ChunkHeader(stream)
+    unknown = (header.unknown0, header.unknown1)
+    # sometimes there are chunks with all 0 for some reason
+    if header.subfileHeader.magic == 0x0:
+        return None
+    errors = 0
     chunk_sections = {}
-    data_left = bytes(parsed.data)
+    data_left = stream.read(subfile_size - header.size)
     total = len(data_left)
-    for chunk_section in parsed.header.sections:
-        # print(chunk_section)
+    for chunk_section in header.sections:
         if chunk_section.index == -1:
             continue
         decompress_object = zlib.decompressobj()
         # print(f"0x{total - len(data_left):X}/0x{total:X} bytes parsed")
 
-        decompressed_chunk = decompress_object.decompress(data_left)
+        try:
+            decompressed_chunk = decompress_object.decompress(data_left)
+        except Exception:
+            print(f"decompression error", file=sys.stderr)
+            errors += 1
+            # traceback.print_exc()
+            continue
         chunk_sections[chunk_section.index] = decompressed_chunk
         data_left = decompress_object.unused_data
-    return chunk_sections, parsed.header.unknown
+    # if header.unknown0 != 0x3:
+    #     input(repr(header))
+    return (chunk_sections, unknown, errors)
 
 
-def parse_cdb_stream(cdb):
+def parse_cdb_stream(stream):
+    old_seek = stream.tell()
+    file_header = parser.FileHeader(stream)
+    stream.seek(old_seek)
     chunks = []
-    for chunk_index in range(40):
-        raw_chunk = cdb.read(parser.chunk.size)
-        chunks.append(parse_chunk(raw_chunk))
+    for chunk_index in range(file_header.subfileCount):
+        chunks.append(parse_chunk(stream, file_header.subfileSize))
     return chunks
 
 
@@ -112,25 +127,26 @@ def main():
     script_path = Path(__file__).parent
     out_path = script_path / "out"
     if out_path.exists():
-        print(
-            'already extracted, please move or delete the "out" folder', file=sys.stderr
-        )
-        sys.exit(1)
-    out_path.mkdir()
+        if 1:
+            shutil.rmtree(out_path)
+        else:
+            print(
+                'already extracted, please move or delete the "out" folder',
+                file=sys.stderr,
+            )
+            sys.exit(1)
     world_input = input("Enter path to world: ")
     cdb_files = get_cdb_files(script_path / world_input)
+    out_path.mkdir()
     for number, cdb_file in cdb_files.items():
         region_path = out_path / f"region{number:d}"
         region_path.mkdir()
-        try:
-            chunks = parse_cdb(cdb_file)
-        except Exception:
-            traceback.print_exc()
-            print(f"error parsing region {number:d}", file=sys.stderr)
-            continue
-        else:
-            print(f"extracted region {number:d}!")
+        print(f"parsing region {number:d}")
+        chunks = parse_cdb(cdb_file)
+        print(f"extracted region {number:d}!")
         for index, chunk in enumerate(chunks):
+            if chunk is None:
+                continue
             chunk_path = region_path / f"chunk{index:d}"
             chunk_path.mkdir()
             for section_index, chunk_section in chunk[0].items():
