a35416534a9298778e0fd3f17e319fe064b48505
Anonymous941 <36797492+Anonymous941@users.noreply.github.com>
2024-09-04 17:13:07 -0400

    make script safer

--- mc3ds/__main__.py
+++ mc3ds/__main__.py
@@ -12,43 +12,50 @@ from .parser import *
 
 
 @click.command()
-@click.argument("path", type=click.Path(exists=True, path_type=Path), required=False)
+@click.argument(
+    "path",
+    type=click.Path(exists=True, file_okay=False, path_type=Path),
+    required=False,
+)
 @click.option(
     "-o",
     "--out",
-    type=click.Path(path_type=Path),
+    type=click.Path(file_okay=False, path_type=Path),
     default="out",
     help="Directory for output",
 )
-@click.option("-n", "--no-delete-out", help="Don't delete the out folder if it exists")
-def main(path: Path, out: Path, no_delete_out: bool = False) -> None:
+@click.option(
+    "-d",
+    "--delete-out",
+    help="Permanently delete the out folder and its contents if it exists (make sure it's the right folder!)",
+)
+def main(path: Path, out: Path, delete_out: bool = False) -> None:
     script_path = Path(__file__).parent
-    out_path = script_path / "out"
-    if out_path.exists():
-        if no_delete_out:
+    if out.exists():
+        if delete_out:
+            shutil.rmtree(out)
+        else:
             print(
                 'already extracted, please move or delete the "out" folder',
                 file=sys.stderr,
             )
             sys.exit(1)
-        else:
-            shutil.rmtree(out_path)
 
     if path is None:
         path = Path(input("Enter path to world: "))
     world = World(path)
     print(f"World name: {world.name}")
-    out_path.mkdir()
+    out.mkdir()
     for number, cdb_file in world.cdb:
-        region_path = out_path / f"region{number:d}"
+        region_path = out / f"region{number:d}"
         region_path.mkdir()
         for index, chunk in cdb_file:
             chunk_path = region_path / f"chunk{index:d}"
             chunk_path.mkdir()
             for subchunk_index, subchunk in chunk:
                 subchunk_path = chunk_path / f"subchunk{subchunk_index:d}"
-                with open(subchunk_path, "wb") as out:
-                    out.write(subchunk.decompressed)
+                with open(subchunk_path, "wb") as subchunk_out:
+                    subchunk_out.write(subchunk.decompressed)
         print(f"extracted region {number:d}!")
 
 
